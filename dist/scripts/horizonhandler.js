2568b90bef8c08527057c9ab617b3acb33e6176e
Jbiloki <bilokij@gmail.com>
2017-03-30 12:40:40 -0700

    DB Added

--- dist/scripts/horizonhandler.js
+++ dist/scripts/horizonhandler.js
@@ -1,8 +1,37 @@
-(function(){
+(function(window){
     'use strict';
+    var App = window.App || {};
     var horizon = Horizon();
-    horizon.onReady(function() {
-        console.log("Database Connection Established,");
-    });
+    var $ = window.jQuery;
+    const chat = horizon('messages');
+
+    function CreateDB(){
+      horizon.onReady(function() {
+          console.log("Database Connection Established.");
+      });
+    }
+    CreateDB.prototype.createMessage = function(text, postName) {
+        let message = {
+          text: text,
+          datetime: new Date(),
+          author: postName
+        }
+        chat.store(message);
+    }
+
+    CreateDB.prototype.getMessage = function(){
+      chat.fetch().subscribe(
+        (items) => {
+          items.forEach((item) => {
+            console.log(item);
+          })
+        },
+
+        (err) => {
+          console.log(err);
+        })
+    }
     horizon.connect();
-}());
+    App.CreateDB = CreateDB;
+    window.App = App;
+})(window);
---
+++ dist/scripts/main.js
@@ -0,0 +1,6 @@
+(function(window) {
+    'use strict';
+    var App = window.App;
+    var DataBase = App.CreateDB;
+    
+})(window);
