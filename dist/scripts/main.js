bf66044407278c00c611f0cfbf9b2c473590d9b3
yaserig <mr.yaserig@gmail.com>
2017-03-31 22:55:29 -0700

    minor fixes, added alternating text/jokes in cover

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -8,3 +8,29 @@
     Data.submitHandler();
 
 })(window);
+
+
+// sentences/ jokes to display in the cover image, feel free to add/remove any!
+var sentences = [
+    'Hello World!',
+    'What is a martians favorite chocolate bar? ',
+    'A Mars bar',
+    'another joke?',
+    'ok!',
+    'What do aliens like to read?',
+    'Comet books!',
+    'haa!',
+];
+
+var interval = 1300;
+
+var holder1 = $('.holder-1');
+var currentIndex = 0;
+
+function doIt() {
+    holder1.html(sentences[currentIndex]);
+    currentIndex = (currentIndex + 1) % sentences.length;
+    setTimeout(doIt, interval);
+}
+
+doIt();
